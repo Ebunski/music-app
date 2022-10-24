eca8cc4d981f766afe60dbc473533de91202d1d1
Oyebola Ebunoluwa <oyebolaebun07@gmail.com>
2022-10-24 22:49:25 +0100

    layout fixed

--- src/components/SharedLayout.jsx
+++ src/components/SharedLayout.jsx
@@ -1,10 +1,12 @@
 import Nav from "../components/Nav";
 import Player from "../components/Player";
+import { Outlet } from "react-router-dom";
 
 export default function SharedLayout() {
   return (
     <div className="App">
       <Nav />
+      <Outlet />
       <Player />
     </div>
   );
